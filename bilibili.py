2b5cf8272b525519c4d69b19be2fe1353da5cead
herrrrr <41541836+herrrrr@users.noreply.github.com>
2018-07-22 20:32:39 +0800

    Create bilibili.py

---
+++ bilibili.py
@@ -0,0 +1,14 @@
+import urllib.request 
+import re   
+from bs4 import BeautifulSoup  
+
+response = urllib.request.urlopen('http://h.bilibili.com/2807583')
+d = response.read()
+soup = BeautifulSoup(d,"html.parser")
+print(soup)
+actList = []
+for lind in soup.find_all('div', class_='info'):
+    actid = lind.find('span', class_='floor').get_text()
+    print(actid)
+    if actid:
+        actList.append(str(actid))
