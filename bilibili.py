a0e9feaa96b7239c4b37153880273a8fcb067db9
hermit <41541836+herrrrr@users.noreply.github.com>
2018-07-30 19:40:56 +0800

    更新

--- bilibili.py
+++ bilibili.py
@@ -2,7 +2,7 @@ import urllib.request
 import re   
 from bs4 import BeautifulSoup  
 
-response = urllib.request.urlopen('http://h.bilibili.com/2807583')
+response = urllib.request.urlopen('http://h.bilibili.com/xxxxx')
 d = response.read()
 soup = BeautifulSoup(d,"html.parser")
 print(soup)
